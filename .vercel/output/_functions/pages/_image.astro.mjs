7b0e2cdf92d2421509ebf7644019643e08efb7c8
victus-ubuntu-santy <santymc@icloud.com>
2025-03-14 14:31:13 +0000

    New try to fix the vercel bug

--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_Q1A6ApgI.mjs';
-export { renderers } from '../renderers.mjs';
--- src/pages/api/prueba.ts
+++
@@ -1,5 +0,0 @@
-import type { APIRoute } from "astro";
-
-export const POST: APIRoute = async ({ params, request }) => {
-    return new Response("Hello world", {status: 200})
-}  
