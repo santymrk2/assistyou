6b68d15f8ccb6428dda3c962e211c33b01f787ad
victus-ubuntu-santy <santymc@icloud.com>
2025-03-14 01:23:10 +0000

    New turso conection

--- db/config.ts
+++ db/config.ts
@@ -1,46 +1,114 @@
-import { column, defineDb, defineTable} from 'astro:db';
+import { column, defineDb, defineTable } from 'astro:db';
 
-
-const School = defineTable({
-  columns: { 
-    id: column.text({primaryKey: true}),
-    name: column.text()
+// Tabla de Usuarios
+const AppUser = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    username: column.text()
   }
-})
+});
 
+// Tabla de Escuelas
+const School = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    name: column.text(),
+    user_id: column.text({ references: () => AppUser.columns.id })
+  }
+});
+
+// Tabla de Cursos
 const Course = defineTable({
   columns: {
-    id: column.text({primaryKey: true}),
+    id: column.text({ primaryKey: true }),
     name: column.text(),
-    school_course: column.text({references: () => School.columns.id})
+    school_id: column.text({ references: () => School.columns.id }),
+    user_id: column.text({ references: () => AppUser.columns.id })
   }
-})
+});
 
-const Student = defineTable({
-  columns: { 
-    id: column.text({primaryKey: true}),
-    name: column.text(),
-    course_student: column.text({references: () => Course.columns.id})  
-  }
-})
-
-
-const Attendance = {
+// Tabla de Cuatrimestres (SemesterPeriod)
+const SemesterPeriod = defineTable({
   columns: {
-    student_id: column.text({references: () => Student.columns.id}),
-    date: column.date(),
-    status: column.text()
+    id: column.number({ primaryKey: true}),
+    course_id: column.text({ references: () => Course.columns.id }),
+    name: column.text(),              // Ejemplo: 'Primer Cuatrimestre'
+    start_date: column.date(),        // O column.text(), según convenga
+    end_date: column.date(),
+    user_id: column.text({ references: () => AppUser.columns.id })
   }
-};
+});
 
+// Tabla de Estudiantes
+const Student = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    name: column.text(),
+    course_id: column.text({ references: () => Course.columns.id }),
+    user_id: column.text({ references: () => AppUser.columns.id })
+  }
+});
 
+// Tabla de Asistencia
+const Attendance = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    course_id: column.text({ references: () => Course.columns.id }),
+    attendance_date: column.date(), // Representa la fecha de asistencia
+    semester_period_id: column.number({
+      references: () => SemesterPeriod.columns.id,
+    }),
+    user_id: column.text({ references: () => AppUser.columns.id })
+  }
+});
 
-// https://astro.build/db/config
+// Tabla de Exámenes
+const Exam = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    name: column.text(),      // Nombre del examen
+    exam_date: column.date(),
+    course_id: column.text({ references: () => Course.columns.id }),
+    semester_period_id: column.number({
+      references: () => SemesterPeriod.columns.id,    }),
+    user_id: column.text({ references: () => AppUser.columns.id })
+  }
+});
+
+// Tabla para relacionar Asistencia con Estudiante (AttendanceStudent)
+const AttendanceStudent = defineTable({
+  columns: {
+    attendance_id: column.number({ references: () => Attendance.columns.id }),
+    student_id: column.text({ references: () => Student.columns.id }),
+    user_id: column.text({ references: () => AppUser.columns.id })
+  },
+  indexes: [{on:['attendance_id', 'student_id'], unique: true}
+]
+});
+
+// Tabla para relacionar Exámenes con Estudiante (ExamStudent)
+const ExamStudent = defineTable({
+  columns: {
+    exam_id: column.number({ references: () => Exam.columns.id }),
+    student_id: column.text({ references: () => Student.columns.id }),
+    nota: column.number(), // Nota, puede ser nula
+    user_id: column.text({ references: () => AppUser.columns.id })
+  },
+  indexes: [
+    { on: ["exam_id", "student_id"], unique: true}
+  ]
+});
+
 export default defineDb({
   tables: {
-    Course,
+    AppUser,
     School,
+    Course,
+    SemesterPeriod,
     Student,
-    Attendance
+    Attendance,
+    Exam,
+    AttendanceStudent,
+    ExamStudent
   }
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,56 +1,65 @@
-import { db, School, Course, Student } from 'astro:db';
+import { db,  AppUser, School, Course, Student, SemesterPeriod, Attendance, Exam, AttendanceStudent, ExamStudent} from 'astro:db';
 
-// https://astro.build/db/seed
 export default async function seed() {
+  // Inserta un usuario para las referencias
+  await db.insert(AppUser).values([{ id: 'admin', username: 'admin'}]);
+
+
   await db.insert(School).values([
-    { id: '1', name: 'Godspell' },
-    { id: '2', name: 'SanPablo' },
-    { id: '3', name: 'Complejo' },
+    { id: 'sanpablo', name: 'San Pablo', user_id: 'admin'},
+    { id: 'godspell', name: 'Godspell', user_id: 'admin'},
+    { id: 'complejo', name: 'Complejo', user_id: 'admin'},
   ]);
 
   await db.insert(Course).values([
-    { id: '1', name: '3A', school_course: '2' },
-    { id: '2', name: '3B', school_course: '2' },
-    { id: '3', name: '6U', school_course: '3' },
+    { id: '6A', name: '6A', school_id: 'sanpablo', user_id: 'admin'},
+    { id: '5A', name: '5A', school_id: 'godspell', user_id: 'admin'},
+    { id: '3B', name: '3B', school_id: 'complejo', user_id: 'admin'},
+  ]);
+
+  await db.insert(SemesterPeriod).values([
+    { id:1, course_id: '6A', name: 'Primer Cuatrimestre', start_date: new Date(), end_date: new Date(), user_id: 'admin'},
+    { id:2, course_id: '5A', name: 'Primer Cuatrimestre', start_date: new Date(), end_date: new Date(), user_id: 'admin'},
+    { id:3, course_id: '3B', name: 'Primer Cuatrimestre', start_date: new Date(), end_date: new Date(), user_id: 'admin'},
   ]);
 
   await db.insert(Student).values([
-    { id: '1', name: 'Alfred', course_student: '2' },
-    { id: '2', name: 'Bilbo', course_student: '2' },
-    { id: '4', name: 'ALEGRE, Belen Ailin', course_student: '3' },
-    { id: '5', name: 'ANTA, Milena Nicole', course_student: '3' },
-    { id: '6', name: 'APANG, Valentin', course_student: '3' },
-    { id: '7', name: 'AVALOS, Melina Magalì', course_student: '3' },
-    { id: '8', name: 'AYALA GODOY, Liz Melina', course_student: '3' },
-    { id: '9', name: 'BAGINAY, Mauro Iván', course_student: '3' },
-    { id: '10', name: 'BENITEZ, Jimena Geraldine', course_student: '3' },
-    { id: '11', name: 'CANTERO ZARATE, Liz Paola', course_student: '3' },
-    { id: '12', name: 'CANTERO ZARATE, Stella Maris', course_student: '3' },
-    { id: '13', name: 'CARBONE GONZALEZ, Gian Franco', course_student: '3' },
-    { id: '14', name: 'CEQUEIRA, Costanza Abigail', course_student: '3' },
-    { id: '15', name: 'CHIPANA CISNEROS, Facundo Ariel', course_student: '3' },
-    { id: '16', name: 'DOYHENART, Francisco Tobias', course_student: '3' },
-    { id: '17', name: 'DURAND, Jeremias Santiago', course_student: '3' },
-    { id: '18', name: 'FERNANDEZ, Angeles Morena', course_student: '3' },
-    { id: '19', name: 'GAUNA, Lourdes Abril', course_student: '3' },
-    { id: '20', name: 'GUEVARA, Santiago Damián', course_student: '3' },
-    { id: '21', name: 'HERRERA, Maia Berenice', course_student: '3' },
-    { id: '22', name: 'IBARRA HUAYHUA, Marta Jacqueline', course_student: '3' },
-    { id: '23', name: 'LENCINA, Thiago Liam', course_student: '3' },
-    { id: '24', name: 'LEONOV, Mateo Josue', course_student: '3' },
-    { id: '25', name: 'LLAVE QUENA, Victoria Noemi', course_student: '3' },
-    { id: '26', name: 'MANRRIQUE, Patricio Valentín', course_student: '3' },
-    { id: '27', name: 'MANSILLA SOTO, Tatiana Yazmin', course_student: '3' },
-    { id: '28', name: 'PADOANI, Fiamma Roquelina', course_student: '3' },
-    { id: '29', name: 'PALAVECINO, Rocio Micaela', course_student: '3' },
-    { id: '30', name: 'ROMERO, Malena Elina', course_student: '3' },
-    { id: '31', name: 'ROSAS, Jonatan Isaias', course_student: '3' },
-    { id: '32', name: 'SCHIMPF, Erica Lucia', course_student: '3' },
-    { id: '33', name: 'SCHMIDT, Luciano Tomas', course_student: '3' },
-    { id: '34', name: 'SILVA, Samira Belen', course_student: '3' },
-    { id: '35', name: 'SPIOUSSAS, Dafne Martina', course_student: '3' },
-    { id: '36', name: 'STURLA, Julieta Candela', course_student: '3' },
-    { id: '37', name: 'TESTATONDA, Heber Adriel', course_student: '3' },
-    { id: '38', name: 'VEGA OLIVERA, Morena Agostina', course_student: '3' },
+    { id: 'S6A1', name: 'Alumno 6A 1', course_id: '6A', user_id: 'admin'},
+    { id: 'S6A2', name: 'Alumno 6A 2', course_id: '6A', user_id: 'admin'},
+    { id: 'S5A1', name: 'Alumno 5A 1', course_id: '5A', user_id: 'admin'},
+    { id: 'S5A2', name: 'Alumno 5A 2', course_id: '5A', user_id: 'admin'},
+    { id: 'S3B1', name: 'Alumno 3B 1', course_id: '3B', user_id: 'admin'},
+    { id: 'S3B2', name: 'Alumno 3B 2', course_id: '3B', user_id: 'admin'},
   ]);
+
+  await db.insert(Attendance).values([
+    { id: 1, course_id: '6A', attendance_date: new Date(), semester_period_id: 1, user_id: 'admin'},
+    { id: 2, course_id: '5A', attendance_date: new Date(), semester_period_id: 2, user_id: 'admin'},
+    { id: 3, course_id: '3B', attendance_date: new Date(), semester_period_id: 3, user_id: 'admin'},
+  ]);
+
+  await db.insert(AttendanceStudent).values([
+    { attendance_id: 1, student_id: 'S6A1', user_id: 'admin'},
+    { attendance_id: 1, student_id: 'S6A2', user_id: 'admin'},
+    { attendance_id: 2, student_id: 'S5A1', user_id: 'admin'},
+    { attendance_id: 2, student_id: 'S5A2', user_id: 'admin'},
+    { attendance_id: 3, student_id: 'S3B1', user_id: 'admin'},
+    { attendance_id: 3, student_id: 'S3B2', user_id: 'admin'},
+  ]);
+
+  await db.insert(Exam).values([
+    { id: 1, name: 'Examen Parcial 1', exam_date: new Date(), course_id: '6A', semester_period_id: 1, user_id: 'admin'},
+    { id: 2, name: 'Examen Parcial 1', exam_date: new Date(), course_id: '5A', semester_period_id: 2, user_id: 'admin'},
+    { id: 3, name: 'Examen Parcial 1', exam_date: new Date(), course_id: '3B', semester_period_id: 3, user_id: 'admin'},
+  ]);
+
+  await db.insert(ExamStudent).values([
+    { exam_id: 1, student_id: 'S6A1', nota: 8.5, user_id: 'admin'},
+    { exam_id: 1, student_id: 'S6A2', nota: 7.0, user_id: 'admin'},
+    { exam_id: 2, student_id: 'S5A1', nota: 9.0, user_id: 'admin'},
+    { exam_id: 2, student_id: 'S5A2', nota: 6.5, user_id: 'admin'},
+    { exam_id: 3, student_id: 'S3B1', nota: 8.0, user_id: 'admin'},
+    { exam_id: 3, student_id: 'S3B2', nota: 7.5, user_id: 'admin'},
+  ]);
+
 }
